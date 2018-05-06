d100ba0629ab2d9dc69f9f5888f90c06bcfeb469
Christiana Vandermale <christianavdm@gmail.com>
2018-05-06 17:36:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -6,8 +6,7 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
 
-  holiday_hash[:winter][:christmas] << supply
-  holiday_hash[:winter][:new_years] << supply
+  holiday_hash[:winter].each do 
 
 end
 
